f52d9084c10fcf7eb1372eb96787ab58237ca49a
ali-br <alibolouri1380@gmail.com>
2022-04-05 22:32:44 +0430

    added dashboard

---
+++ src/pages/Dashboard/index.jsx
@@ -0,0 +1,14 @@
+import React,{ useState, useEffect } from "react";
+import { Row, Col } from "antd";
+import { Line } from '@ant-design/plots';
+const Dashboard = () => {
+	return (
+		<Row>
+			<Col>
+
+            </Col>
+		</Row>
+	);
+};
+
+export default Dashboard;
