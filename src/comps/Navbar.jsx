31de5e8417a3f65b9aeea9953dc57a8e61dae303
AmirHossein Rezaei <DinonowDev@gmail.com>
2022-04-06 23:39:24 +0430

    change in navbar

--- src/comps/Navbar.jsx
+++ src/comps/Navbar.jsx
@@ -2,23 +2,21 @@ import React from "react";
 import classes from "./Navbar.module.scss";
 import traderImg from "../assets/trader.jpg";
 import logo from "../assets/logo.svg";
-import {FaPowerOff} from "react-icons/fa";
+import { FaPowerOff } from "react-icons/fa";
 const Navbar = () => {
-	return (
-		<div className={classes.nav}>
-			<div className={classes.trader}>
-				<img src={traderImg} alt='' />
-				<h3>علی بلوری</h3>
-			</div>
-			<div className={classes.logo}>
-				<img src={logo} alt='' />
-			</div>
-			<div>
-				{/* <button className={classes.button}>خروج</button> */}
-				<FaPowerOff className={classes.icon} />
-			</div>
-		</div>
-	);
+  return (
+    <div className={classes.nav}>
+      <div className={classes.trader}>
+        <img src={traderImg} alt="" />
+        <h3>علی بلوری</h3>
+      </div>
+      <div className={classes.logo}>
+        <img src={logo} alt="" />
+      </div>
+      {/* <button className={classes.button}>خروج</button> */}
+      <FaPowerOff className={classes.icon} />
+    </div>
+  );
 };
 
 export default Navbar;
