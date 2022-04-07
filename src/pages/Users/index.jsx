58ffec2c53cc471a3a895f978352504fd0c87192
AmirHossein Rezaei <DinonowDev@gmail.com>
2022-04-07 22:09:21 +0430

    some change

---
+++ src/pages/Users/index.jsx
@@ -0,0 +1,57 @@
+const Users = () => {
+  const [data, setData] = useState([
+    {
+      email: "DinonowDev@gmail.com",
+      balance: 10000,
+      ProfitAndLoss: -52,
+      accountType: "FTMO",
+      platform: "MT4",
+    },
+    {
+      email: "DinonowDev@gmail.com",
+      balance: 10000,
+      ProfitAndLoss: -52,
+      accountType: "FTMO",
+      platform: "MT4",
+    },
+    {
+      email: "DinonowDev@gmail.com",
+      balance: 10000,
+      ProfitAndLoss: -52,
+      accountType: "FTMO",
+      platform: "MT4",
+    },
+    {
+      email: "DinonowDev@gmail.com",
+      balance: 10000,
+      ProfitAndLoss: -52,
+      accountType: "FTMO",
+      platform: "MT4",
+    },
+    {
+      email: "DinonowDev@gmail.com",
+      balance: 10000,
+      ProfitAndLoss: -52,
+      accountType: "FTMO",
+      platform: "MT4",
+    },
+    {
+      email: "DinonowDev@gmail.com",
+      balance: 10000,
+      ProfitAndLoss: -52,
+      accountType: "FTMO",
+      platform: "MT4",
+    },
+    {
+      email: "DinonowDev@gmail.com",
+      balance: 10000,
+      ProfitAndLoss: -52,
+      accountType: "FTMO",
+      platform: "MT4",
+    },
+  ]);
+
+  return <></>;
+};
+
+export default Users;
