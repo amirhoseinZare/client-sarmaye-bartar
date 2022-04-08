633a755a6d3dcdc0e349ad27da0bfa05613000b1
AmirHossein Rezaei <DinonowDev@gmail.com>
2022-04-08 19:19:15 +0430

    add objective api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import "./assets/fonts/font.scss";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "antd/dist/antd.css";
 import { ConfigProvider } from "antd";
-import "./assets/fonts/font.scss";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
