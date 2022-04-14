556f028f38cb5009205ab2c9aa4d8483adc6a1f3
AmirHossein Rezaei <DinonowDev@gmail.com>
2022-04-14 14:21:45 +0430

    push static data

--- src/redux/reducers/alert.js
+++ src/redux/reducers/alert.js
@@ -1,29 +1,41 @@
 import alertTypes from "../types/alert";
 
-const INITIAL_STATE = []
+const INITIAL_STATE = [
+  {
+    time: "time",
+    message: "message",
+    title: "title",
+    read: true,
+    type: "type",
+    id: `time`,
+  },
+];
 
 const alertReducer = (state = INITIAL_STATE, action) => {
-	switch (action.type) {
-		case alertTypes.ADD_ALERT: {
-			const { time, message, title, type } = action.payload;
+  switch (action.type) {
+    case alertTypes.ADD_ALERT: {
+      const { time, message, title, type } = action.payload;
 
-			return [...state, {
-				time: time,
-				message: message,
-				title:title,
-				read:true,
-                type,
-                id:`${time}`
-			}]
-		}
-        case alertTypes.READ_ALERT: {
-			const { id } = action.payload;
-            const newAlerts = state.filter(alert=>alert.id!==id)
-			return [ ...newAlerts ]
-		}
-		default:
-			return state;
-	}
+      return [
+        ...state,
+        {
+          time: time,
+          message: message,
+          title: title,
+          read: true,
+          type,
+          id: `${time}`,
+        },
+      ];
+    }
+    case alertTypes.READ_ALERT: {
+      const { id } = action.payload;
+      const newAlerts = state.filter((alert) => alert.id !== id);
+      return [...newAlerts];
+    }
+    default:
+      return state;
+  }
 };
 
 export default alertReducer;
