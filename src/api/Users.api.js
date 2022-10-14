341181e2410018645ae11a526db46f4f4e9df034
amir <73912042+amirhoseinZare@users.noreply.github.com>
2022-10-15 00:49:52 +0330

    fix filters in users page

--- src/api/Users.api.js
+++ src/api/Users.api.js
@@ -3,17 +3,14 @@ import http from "./http.api";
 const UsersApi = new http("/user");
 
 UsersApi.gets = null;
-UsersApi.getOne = UsersApi.get;
+UsersApi.get = null;
 UsersApi.post = null;
 UsersApi.patch = null;
 UsersApi.delete = null;
 
-UsersApi.all = function (pageSize, pageNumber, user_email="") {
-  return this.instance.get(
-    `${this.baseApisUrl}?pageSize=${pageSize}&pageNumber=${pageNumber}&user_email=${user_email}`
-  );
+UsersApi.all = function (config) {
+  return this.instance.get(`${this.baseApisUrl}`, {params:config});
 };
-
 UsersApi.delUser = function (id) {
   return this.instance.delete(`${this.baseApisUrl}/${id}`);
 };
